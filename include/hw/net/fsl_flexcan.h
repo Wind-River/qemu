5b23ca0ff6ca977f8203bf0aeeed50a5f6750095
Nelson Ho <Nelson.Ho@windriver.com>
2024-04-15 22:09:28 +0000

    hw/net/can/fsl_flexcan: initial commit
    Define basic data structures and registers used by vxworks
    flexcan driver initialization.
    
    Signed-off-by: Nelson Ho <Nelson.Ho@windriver.com>

---
+++ include/hw/net/fsl_flexcan.h
@@ -0,0 +1,42 @@
+/*
+ * QEMU model of Freescale FlexCAN Controller
+ *
+ * Copyright (C) 2024 Wind River Systems, Inc.
+ *
+ * Written-by: Nelson Ho <nelson.ho@windriver.com>
+ *
+ * This program is free software; you can redistribute it and/or modify it
+ * under the terms and conditions of the GNU General Public License,
+ * version 2 or later, as published by the Free Software Foundation.
+ *
+ * This program is distributed in the hope it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or
+ * FITNESS FOR A PARTICULAR PURPOSE.  See the GNU General Public License for
+ * more details.
+ *
+ * You should have received a copy of the GNU General Public License along with
+ * this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+#ifndef HW_FLEXCAN_FSL_H
+#define HW_FLEXCAN_FSL_H
+
+#include "hw/register.h"
+#include "net/can_emu.h"
+#include "hw/qdev-clock.h"
+
+#define TYPE_FSL_FLEXCAN "fsl.flexcan"
+
+OBJECT_DECLARE_SIMPLE_TYPE(FslFlexCanState, FSL_FLEXCAN)
+
+// this is the highest offset register in the mmio space
+#define OFFSET_ERFFEL127    0x31FC
+
+// 4 byte wide registers
+#define FLEXCAN_R_MAX       (OFFSET_ERFFEL127 / 4) + 1
+
+typedef struct FslFlexCanState {
+    uint32_t        regs[FLEXCAN_R_MAX];
+} FslFlexCanState;
+
+#endif // HW_FLEXCAN_FSL_H
