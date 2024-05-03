d927fc87b624d2f3095f014bba6ef241aaa36ae2
Nelson Ho <Nelson.Ho@windriver.com>
2024-05-03 09:26:19 +0000

    hw/net/fsl_flexcan: refactor to use multiple memory regions
    This change refactors fsl_flexcan.c to model the devices mmio space
    more accurately. Previously we modeled the entire mmio space as
    registers, but this was inaccurate since only the first 128 bytes
    of the device mmio space is backed by registers. The remaining space
    is backed by embedded ram.
    
    After this change, the first 128 bytes of device mmio space is modeled
    as registers, and the remaining is modeled using generic mmio.
    
    This change also includes some preparation work for implementing the
    message buffers used during transmit and receive. Since these buffers
    are memory mapped ram on the real device, we refrain from modeling
    these message buffers as registers.
    
    Signed-off-by: Nelson Ho <Nelson.Ho@windriver.com>

--- include/hw/net/fsl_flexcan.h
+++ include/hw/net/fsl_flexcan.h
@@ -29,20 +29,42 @@
 
 OBJECT_DECLARE_SIMPLE_TYPE(FslFlexCanState, FSL_FLEXCAN)
 
-// this is the highest offset register in the mmio space
-#define OFFSET_ERFFEL127    0x31FC
+/*
+ * From RMS32G3 Rev 3.1 Section 51.6.1
+ * The address space occupied by FlexCAN has 128 bytes for registers
+ * starting at the module base address, followed by embedded RAM
+ * starting at address offset 0080h.
+ */
 
-// 4 byte wide registers
-#define FLEXCAN_R_MAX       (OFFSET_ERFFEL127 / 4) + 1
+// 32 4 byte wide registers
+#define FLEXCAN_R_MAX       32
+
+// 32 * 4 bytes worth of register space starting at module base addr
+#define FLEXCAN_R_SZ        FLEXCAN_R_MAX * 4
+
+// 128 16 byte message buffers
+#define FLEXCAN_MB_SZ       128 * 16
+
+// message buffer start offset
+#define FLEXCAN_MB_BASE     0x80
+
+// this is the size advertised in the device tree
+#define FLEXCAN_MMIO_SZ     0xA000
 
 typedef struct FslFlexCanState {
     SysBusDevice        parent_obj;
-    MemoryRegion        iomem;
-    qemu_irq            canfd_irq;
+    MemoryRegion        flexcan;
+    MemoryRegion        reg_mem;
+    MemoryRegion        mb_mem;
+    qemu_irq            irq_state;
+    qemu_irq            irq_berr;
+    qemu_irq            irq_mb_0_7;
+    qemu_irq            irq_mb_8_127;
     CanBusClientState   bus_client;
     CanBusState         *canbus;
     uint32_t            regs[FLEXCAN_R_MAX];
     RegisterInfo        reg_info[FLEXCAN_R_MAX];
+    uint8_t             mb[FLEXCAN_MB_SZ];
 } FslFlexCanState;
 
 #endif // HW_FLEXCAN_FSL_H
