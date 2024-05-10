8c1331a7f4be4adf04338a245563b5fafecd3b22
Nelson Ho <Nelson.Ho@windriver.com>
2024-05-10 16:13:13 +0000

    hw/net/can/fsl_flexcan: Implement CAN TX
    This change implements transmit functionality for the
    FlexCAN controller for non-FD CAN frames. It is by no
    means complete, but it is enough to satisfy the VxWorks
    driver. Future changes will implement non-FD RX, CANFD
    TX, and CANFD RX.
    
    Signed-off-by: Nelson Ho <Nelson.Ho@windriver.com>

--- hw/net/can/fsl_flexcan.c
+++ hw/net/can/fsl_flexcan.c
@@ -20,8 +20,12 @@
 
 #include "qemu/osdep.h"
 #include "hw/sysbus.h"
+#include "hw/irq.h"
+#include "qapi/error.h"
 #include "migration/vmstate.h"
 #include "hw/qdev-properties.h"
+#include "net/can_emu.h"
+#include "net/can_host.h"
 #include "hw/net/fsl_flexcan.h"
 
 REG32(MODULE_CONFIGURATION_REGISTER, 0x0)
@@ -64,10 +68,43 @@ REG32(RX_14_MASK_REGISTER, 0x14)
     FIELD(RX_14_MASK_REGISTER, RX14M, 0, 32)
 REG32(RX_15_MASK_REGISTER, 0x18)
     FIELD(RX_15_MASK_REGISTER, RX15M, 0, 32)
+REG32(ERROR_AND_STATUS_1_REGISTER, 0x20)
+    FIELD(ERROR_AND_STATUS_1_REGISTER, ERRINT,      1,  1)
+    FIELD(ERROR_AND_STATUS_1_REGISTER, BOFFINT,     2,  1)
+    FIELD(ERROR_AND_STATUS_1_REGISTER, RX,          3,  1)
+    FIELD(ERROR_AND_STATUS_1_REGISTER, FLTCONF,     4,  2)
+    FIELD(ERROR_AND_STATUS_1_REGISTER, TX,          6,  1)
+    FIELD(ERROR_AND_STATUS_1_REGISTER, IDLE,        7,  1)
+    FIELD(ERROR_AND_STATUS_1_REGISTER, RXWRN,       8,  1)
+    FIELD(ERROR_AND_STATUS_1_REGISTER, TXWRN,       9,  1)
+    FIELD(ERROR_AND_STATUS_1_REGISTER, STFERR,      10,  1)
+    FIELD(ERROR_AND_STATUS_1_REGISTER, FRMERR,      11,  1)
+    FIELD(ERROR_AND_STATUS_1_REGISTER, CRCERR,      12,  1)
+    FIELD(ERROR_AND_STATUS_1_REGISTER, ACKERR,      13,  1)
+    FIELD(ERROR_AND_STATUS_1_REGISTER, BIT0ERR,     14,  1)
+    FIELD(ERROR_AND_STATUS_1_REGISTER, BIT1ERR,     15,  1)
+    FIELD(ERROR_AND_STATUS_1_REGISTER, RWRNINT,     16,  1)
+    FIELD(ERROR_AND_STATUS_1_REGISTER, TWRNINT,     17,  1)
+    FIELD(ERROR_AND_STATUS_1_REGISTER, SYNCH,       18,  1)
+    FIELD(ERROR_AND_STATUS_1_REGISTER, BOFFDONEINT, 19,  1)
+    FIELD(ERROR_AND_STATUS_1_REGISTER, ERRINT_FAST, 20,  1)
+    FIELD(ERROR_AND_STATUS_1_REGISTER, ERROVR,      21,  1)
+    FIELD(ERROR_AND_STATUS_1_REGISTER, STFERR_FAST, 26,  1)
+    FIELD(ERROR_AND_STATUS_1_REGISTER, FRMERR_FAST, 27,  1)
+    FIELD(ERROR_AND_STATUS_1_REGISTER, CRCERR_FAST, 28,  1)
+    FIELD(ERROR_AND_STATUS_1_REGISTER, BIT0ERR_FAST, 30, 1)
+    FIELD(ERROR_AND_STATUS_1_REGISTER, BIT1ERR_FAST, 31, 1)
 REG32(INTERRUPT_MASKS_2_REGISTER, 0x24)
     FIELD(INTERRUPT_MASKS_2_REGISTER, BUF63TO32M,   0, 32)
 REG32(INTERRUPT_MASKS_1_REGISTER, 0x28)
     FIELD(INTERRUPT_MASKS_1_REGISTER, BUF31TO0M,    0, 32)
+REG32(INTERRUPT_FLAGS_1_REGISTER, 0x30)
+    FIELD(INTERRUPT_FLAGS_1_REGISTER, BUF0I,        0,  1)
+    FIELD(INTERRUPT_FLAGS_1_REGISTER, BUF4TO1I,     1,  4)
+    FIELD(INTERRUPT_FLAGS_1_REGISTER, BUF5I,        5,  1)
+    FIELD(INTERRUPT_FLAGS_1_REGISTER, BUF6I,        6,  1)
+    FIELD(INTERRUPT_FLAGS_1_REGISTER, BUF7I,        7,  1)
+    FIELD(INTERRUPT_FLAGS_1_REGISTER, BUF31TO8I,    8,  24)
 REG32(CONTROL_2_REGISTER, 0x34)
     FIELD(CONTROL_2_REGISTER, ERRMSK_FAST,  31, 1)
     FIELD(CONTROL_2_REGISTER, BOFFDONEMSK,  30, 1)
@@ -85,24 +122,38 @@ REG32(CONTROL_2_REGISTER, 0x34)
     FIELD(CONTROL_2_REGISTER, EDFLTDIS,     11, 1)
     FIELD(CONTROL_2_REGISTER, MBTSBASE,     8,  2)
     FIELD(CONTROL_2_REGISTER, TSTAMPCAP,    6,  2)
-REG32(MEMORY_ERROR_CONTROL_REGISTER, 0xAE0)
-    FIELD(MEMORY_ERROR_CONTROL_REGISTER, ECRWRDIS,  31, 1)
-    FIELD(MEMORY_ERROR_CONTROL_REGISTER, HAERRIE,   15, 1)
-    FIELD(MEMORY_ERROR_CONTROL_REGISTER, FAERRIE,   14, 1)
-    FIELD(MEMORY_ERROR_CONTROL_REGISTER, EXTERRIE,  13, 1)
-    FIELD(MEMORY_ERROR_CONTROL_REGISTER, RERRDIS,   9,  1)
-    FIELD(MEMORY_ERROR_CONTROL_REGISTER, ECCDIS,    8,  1)
-    FIELD(MEMORY_ERROR_CONTROL_REGISTER, NCEFAFRZ,  7,  1)
-REG32(CAN_FD_CONTROL_REGISTER, 0xC00)
-    FIELD(CAN_FD_CONTROL_REGISTER, FDRATE,  31, 1)
-    FIELD(CAN_FD_CONTROL_REGISTER, MBDSR3,  25, 2)
-    FIELD(CAN_FD_CONTROL_REGISTER, MBDSR2,  22, 2)
-    FIELD(CAN_FD_CONTROL_REGISTER, MBDSR1,  19, 2)
-    FIELD(CAN_FD_CONTROL_REGISTER, MBDSR0,  16, 2)
-    FIELD(CAN_FD_CONTROL_REGISTER, TDCEN,   15, 1)
-    FIELD(CAN_FD_CONTROL_REGISTER, TDCFAIL, 14, 1)
-    FIELD(CAN_FD_CONTROL_REGISTER, TDCOFF,  8,  5)
-    FIELD(CAN_FD_CONTROL_REGISTER, TDCVAL,  0,  6)
+
+/* MECR */
+#define MECR_OFFSET         0xAE0
+#define MECR_NCEFAFRZ       (1 << 7)
+#define MECR_ECCDIS         (1 << 8)
+#define MECR_RERRDIS        (1 << 9)
+#define MECR_EXTERRIE       (1 << 13)
+#define MECR_FAERRIE        (1 << 14)
+#define MECR_HAERRIE        (1 << 15)
+#define MECR_ECRWRDIS       (1 << 31)
+
+/* FDCTRL */
+#define FDCTRL_OFFSET       0xC00
+#define FDCTRL_TDCVAL       (0x3F << 0)
+#define FDCTRL_TDCOFF       (0x1F << 8)
+#define FDCTRL_TDCFAIL      (0x1 << 14)
+#define FDCTRL_TDCEN        (0x1 << 15)
+#define FDCTRL_MBDSR0       (0x3 << 16)
+#define FDCTRL_MBDSR1       (0x3 << 19)
+#define FDCTRL_MBDSR2       (0x3 << 22)
+#define FDCTRL_MBDSR3       (0x3 << 25)
+#define FDCTRL_FDRATE       (0x1 << 31)
+
+static void flexcan_update_irq(FslFlexCanState *s)
+{
+    // INTERRUPT_MASKS_1 and INTERRUPT_FLAGS_1 use 1 bit per message buffer
+    if (s->regs[R_INTERRUPT_MASKS_1_REGISTER] & s->regs[R_INTERRUPT_FLAGS_1_REGISTER]) {
+        qemu_set_irq(s->irq_state, 1);
+    } else {
+        qemu_set_irq(s->irq_state, 0);
+    }
+}
 
 static uint64_t flexcan_reg_prewrite(RegisterInfo *reg, uint64_t val64)
 {
@@ -115,10 +166,26 @@ static void flexcan_mcr_postwrite(RegisterInfo *reg, uint64_t val64)
 {
     FslFlexCanState *s = FSL_FLEXCAN(reg->opaque);
 
-    if (!ARRAY_FIELD_EX32(s->regs, MODULE_CONFIGURATION_REGISTER, MDIS)) {
-        ARRAY_FIELD_DP32(s->regs, MODULE_CONFIGURATION_REGISTER, LPMACK, 0);
-    } else {
+    if (ARRAY_FIELD_EX32(s->regs, MODULE_CONFIGURATION_REGISTER, MDIS)) {
         ARRAY_FIELD_DP32(s->regs, MODULE_CONFIGURATION_REGISTER, LPMACK, 1);
+        ARRAY_FIELD_DP32(s->regs, ERROR_AND_STATUS_1_REGISTER, SYNCH, 1);
+    } else {
+        ARRAY_FIELD_DP32(s->regs, MODULE_CONFIGURATION_REGISTER, LPMACK, 0);
+        ARRAY_FIELD_DP32(s->regs, ERROR_AND_STATUS_1_REGISTER, SYNCH, 0);
+    }
+
+    if (ARRAY_FIELD_EX32(s->regs, MODULE_CONFIGURATION_REGISTER, HALT) &&
+        ARRAY_FIELD_EX32(s->regs, MODULE_CONFIGURATION_REGISTER, FRZ)) {
+        ARRAY_FIELD_DP32(s->regs, MODULE_CONFIGURATION_REGISTER, FRZACK, 1);
+    } else {
+        ARRAY_FIELD_DP32(s->regs, MODULE_CONFIGURATION_REGISTER, FRZACK, 0);
+    }
+
+    if (ARRAY_FIELD_EX32(s->regs, MODULE_CONFIGURATION_REGISTER, MDIS) ||
+        ARRAY_FIELD_EX32(s->regs, MODULE_CONFIGURATION_REGISTER, FRZ)) {
+        ARRAY_FIELD_DP32(s->regs, MODULE_CONFIGURATION_REGISTER, NOTRDY, 1);
+    } else {
+        ARRAY_FIELD_DP32(s->regs, MODULE_CONFIGURATION_REGISTER, NOTRDY, 0);
     }
 
     if (ARRAY_FIELD_EX32(s->regs, MODULE_CONFIGURATION_REGISTER, SOFTRST)) {
@@ -152,25 +219,29 @@ static void flexcan_mcr_postwrite(RegisterInfo *reg, uint64_t val64)
            ERFSR
         */
         {
-            // MDIS bit in MCR is not affected by soft reset
-            uint8_t mdis = ARRAY_FIELD_EX32(s->regs, MODULE_CONFIGURATION_REGISTER, MDIS);
+            // MDIS, NOTRDY, SOFTRST, FRZACK, LPMACK bit in MCR is not affected by soft reset
+            uint32_t save = 0;
+            save |= ARRAY_FIELD_EX32(s->regs, MODULE_CONFIGURATION_REGISTER, MDIS);
+            save |= ARRAY_FIELD_EX32(s->regs, MODULE_CONFIGURATION_REGISTER, NOTRDY);
+            save |= ARRAY_FIELD_EX32(s->regs, MODULE_CONFIGURATION_REGISTER, SOFTRST);
+            save |= ARRAY_FIELD_EX32(s->regs, MODULE_CONFIGURATION_REGISTER, FRZACK);
+            save |= ARRAY_FIELD_EX32(s->regs, MODULE_CONFIGURATION_REGISTER, LPMACK);
             register_reset(&s->reg_info[R_MODULE_CONFIGURATION_REGISTER]);
-            ARRAY_FIELD_DP32(s->regs, MODULE_CONFIGURATION_REGISTER, MDIS, mdis);
+            s->regs[R_MODULE_CONFIGURATION_REGISTER] |= save;
         }
+        register_reset(&s->reg_info[R_ERROR_AND_STATUS_1_REGISTER]);
         register_reset(&s->reg_info[R_INTERRUPT_MASKS_2_REGISTER]);
         register_reset(&s->reg_info[R_INTERRUPT_MASKS_1_REGISTER]);
-        register_reset(&s->reg_info[R_MEMORY_ERROR_CONTROL_REGISTER]);
-        register_reset(&s->reg_info[R_CAN_FD_CONTROL_REGISTER]);
         // clear soft reset bit once registers have been reset
         ARRAY_FIELD_DP32(s->regs, MODULE_CONFIGURATION_REGISTER, SOFTRST, 0);
     }
+}
 
-    if (ARRAY_FIELD_EX32(s->regs, MODULE_CONFIGURATION_REGISTER, HALT) &&
-        ARRAY_FIELD_EX32(s->regs, MODULE_CONFIGURATION_REGISTER, FRZ)) {
-        ARRAY_FIELD_DP32(s->regs, MODULE_CONFIGURATION_REGISTER, FRZACK, 1);
-    } else {
-        ARRAY_FIELD_DP32(s->regs, MODULE_CONFIGURATION_REGISTER, FRZACK, 0);
-    }
+static void flexcan_imask_postwrite(RegisterInfo *reg, uint64_t val64)
+{
+    FslFlexCanState *s = FSL_FLEXCAN(reg->opaque);
+
+    flexcan_update_irq(s);
 }
 
 static const RegisterAccessInfo flexcan_regs_info[] = {
@@ -191,36 +262,128 @@ static const RegisterAccessInfo flexcan_regs_info[] = {
     },{ .name = "RX_15_MASK_REGISTER",
         .addr = A_RX_15_MASK_REGISTER,
         .pre_write = flexcan_reg_prewrite,
+    },{ .name = "ERROR_AND_STATUS_1_REGISTER",
+        .addr = A_ERROR_AND_STATUS_1_REGISTER,
+        .pre_write = flexcan_reg_prewrite,
     },{ .name = "INTERRUPT_MASKS_2_REGISTER",
         .addr = A_INTERRUPT_MASKS_2_REGISTER,
         .pre_write = flexcan_reg_prewrite,
     },{ .name = "INTERRUPT_MASKS_1_REGISTER",
         .addr = A_INTERRUPT_MASKS_1_REGISTER,
         .pre_write = flexcan_reg_prewrite,
+        .post_write = flexcan_imask_postwrite,
     },{ .name = "CONTROL_2_REGISTER",
         .addr = A_CONTROL_2_REGISTER,
         .reset = 0x00100000,
         .pre_write = flexcan_reg_prewrite,
-    },{ .name = "MEMORY_ERROR_CONTROL_REGISTER",
-        .addr = A_MEMORY_ERROR_CONTROL_REGISTER,
-        .reset = 0x800C0080,
-        .pre_write = flexcan_reg_prewrite,
-    },{ .name = "CAN_FD_CONTROL_REGISTER",
-        .addr = A_CAN_FD_CONTROL_REGISTER,
-        .reset = 0x80000100,
+    },{ .name = "INTERRUPT_FLAGS_1_REGISTER",
+        .addr = A_INTERRUPT_FLAGS_1_REGISTER,
+        .w1c = 0xFFFFFFFF,
         .pre_write = flexcan_reg_prewrite,
     }
 };
 
+static ssize_t flexcan_receive(CanBusClientState *client,
+                               const qemu_can_frame *buf,
+                               size_t buf_size)
+{
+    // TODO
+    return 0;
+}
+
+static bool flexcan_can_receive(CanBusClientState *client)
+{
+    // TODO
+    return false;
+}
+
+static CanBusClientInfo flexcan_bus_client_info = {
+    .can_receive = flexcan_can_receive,
+    .receive = flexcan_receive,
+};
+
+static void flexcan_connect_canbus(FslFlexCanState *s, Error **errp)
+{
+    if (s->canbus) {
+        s->bus_client.info = &flexcan_bus_client_info;
+        if (can_bus_insert_client(s->canbus, &s->bus_client) < 0) {
+            error_setg(errp, "fsl_flexcan can_bus_insert_client failed");
+        }
+    }
+}
+
 static void flexcan_mb_write(void *opaque, hwaddr offset,
                              uint64_t value, unsigned size)
 {
+    FslFlexCanState *s = opaque;
+    uint32_t mb_idx = offset >> 2;
+    qemu_can_frame frame;
+    int i;
+
+    // write the new value to s->mb
+    s->mb[mb_idx] = (uint32_t)value;
+
+    // TODO: Currently assuming legacy mode with 8 byte payload / 16 byte messages
+    // MB control lives at offset 0 from the start of each message buffer
+    if (!(offset % FLEXCAN_MB_SIZE)) {
+        // writing to MB control
+        if (!ARRAY_FIELD_EX32(s->regs, MODULE_CONFIGURATION_REGISTER, FRZACK) &&
+            FLEXCAN_MB_CTRL_CODE(s->mb[mb_idx]) == FLEXCAN_MB_TX_CODE_DATA) {
+            frame.can_id = FLEXCAN_MB_CTRL_IDE(s->mb[mb_idx]) ?
+                                FLEXCAN_MB_ID_EXTENDED(s->mb[mb_idx + 1]) :
+                                FLEXCAN_MB_ID_STANDARD(s->mb[mb_idx + 1]);
+            frame.can_dlc = FLEXCAN_MB_CTRL_DLC(s->mb[mb_idx]);
+            // extract the data bytes
+            for (i = 0; i < FLEXCAN_MB_DATA_LEN_LEGACY; i++) {
+                frame.data[i] = (s->mb[mb_idx + (i/4) + 2] >> ((i % 4) * 8)) & 0xFF;
+            }
+            /*
+             * TODO: Currently assuming all writes to messages buffer are TX.
+             *
+             * CTRL2[RFFN] indicates number of legacy RX fifos
+             * switch CTRL2[RFFN]:
+             *      case 0:
+             *          RX MB[0..7]
+             *          TX MB[8..MCR[MAXMB]]
+             *      case 1:
+             *          RX MB[0..9]
+             *          TX MB[10..MCR[MAXMB]]
+             *      case 2:
+             *          RX MB[0..11]
+             *          TX MB[12..MCR[MAXMB]]
+             *      case 3:
+             *          RX MB[0..13]
+             *          TX MB[14..MCR[MAXMB]]
+             *      case 4:
+             *          RX MB[0..15]
+             *          TX MB[16..MCR[MAXMB]]
+             *      ...
+             *
+             *  In legacy (non-FD) mode, the maximum value of MCR[MAXMB] is 127
+             *
+             *  It is the software's responsibility to program MCR[MAXMB]
+             *  to a value that is compatible with the device configuration.
+             *
+             *  VxWorks configures MCR[MAXMB] to 8, so that MB8 is the only
+             *  message buffer used for TX.
+             *  
+             *  VxWorks does not use IFLAGS[2..4] registers since it does not
+             *  use MBs higher than 8.
+             */
+            can_bus_client_send(&s->bus_client, &frame, 1);
+            ARRAY_FIELD_DP32(s->regs, INTERRUPT_FLAGS_1_REGISTER, BUF31TO8I, 1);
+        }
+    }
+    flexcan_update_irq(s);
 }
 
 static uint64_t flexcan_mb_read(void *opaque, hwaddr offset,
                                 unsigned size)
 {
-    return 0;
+    FslFlexCanState *s = opaque;
+    uint32_t mb_idx = offset >> 2;
+
+    return s->mb[mb_idx];
 }
 
 static const MemoryRegionOps flexcan_mb_ops = {
@@ -262,7 +425,7 @@ static void flexcan_realize(DeviceState *dev, Error **errp)
     // add mem region for message buffers to top level memory region
     memory_region_init_io(&s->mb_mem, OBJECT(s), &flexcan_mb_ops,
                           s, "flexcan.mb", FLEXCAN_MB_LEN);
-    memory_region_add_subregion(&s->flexcan, 0x80, &s->mb_mem);
+    memory_region_add_subregion(&s->flexcan, FLEXCAN_MB_BASE, &s->mb_mem);
 
     // init the top level memory region for mmio
     sysbus_init_mmio(SYS_BUS_DEVICE(dev), &s->flexcan);
@@ -272,7 +435,8 @@ static void flexcan_realize(DeviceState *dev, Error **errp)
     sysbus_init_irq(SYS_BUS_DEVICE(dev), &s->irq_berr);
     sysbus_init_irq(SYS_BUS_DEVICE(dev), &s->irq_mb_0_7);
     sysbus_init_irq(SYS_BUS_DEVICE(dev), &s->irq_mb_8_127);
-    // todo: connect s->canbus
+
+    flexcan_connect_canbus(s, errp);
 }
 
 static void flexcan_reset(DeviceState *dev)
