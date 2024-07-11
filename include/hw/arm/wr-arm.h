601fb5cae9cd9e8356ce3266464f7401ad98d4cf
Nelson Ho <Nelson.Ho@windriver.com>
2024-07-11 07:37:17 +0000

    hw/arm/wr-arm.h: add copyright header
    Signed-off-by: Nelson Ho <Nelson.Ho@windriver.com>

--- include/hw/arm/wr-arm.h
+++ include/hw/arm/wr-arm.h
@@ -1,3 +1,20 @@
+/*
+ *
+ * Copyright (C) 2024 Wind River Systems, Inc.
+ *
+ * This program is free software; you can redistribute it and/or modify it
+ * under the terms and conditions of the GNU General Public License,
+ * version 2 or later, as published by the Free Software Foundation.
+ *
+ * This program is distributed in the hope it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or
+ * FITNESS FOR A PARTICULAR PURPOSE.  See the GNU General Public License for
+ * more details.
+ *
+ * You should have received a copy of the GNU General Public License along with
+ * this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
 #ifndef QEMU_WR_ARM_H
 #define QEMU_WR_ARM_H
 
