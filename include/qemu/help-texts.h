86a4b5501800f615ba63176ca0fe35174cbd2b6d
James Hui <huitmj@gmail.com>
2024-11-19 23:05:03 +0000

    adding Wind River footnotes for version and help arguments

--- include/qemu/help-texts.h
+++ include/qemu/help-texts.h
@@ -3,11 +3,13 @@
 
 /* Copyright string for -version arguments, About dialogs, etc */
 #define QEMU_COPYRIGHT "Copyright (c) 2003-2024 " \
-    "Fabrice Bellard and the QEMU Project developers"
+    "Fabrice Bellard and the QEMU Project developers" \
+    "\nCopyright (c) 2023-2024 Wind River Systems, Inc."
 
 /* Bug reporting information for --help arguments, About dialogs, etc */
 #define QEMU_HELP_BOTTOM \
     "See <https://qemu.org/contribute/report-a-bug> for how to report bugs.\n" \
-    "More information on the QEMU project at <https://qemu.org>."
+    "More information on the QEMU project at <https://qemu.org>." \
+    "\nMore information on Wind River QEMU at <http://github.com/Wind-River/qemu"
 
 #endif
