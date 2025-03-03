a407b5329ef29c102f81801bb152a6a776c5a331
Nelson Ho <Nelson.Ho@windriver.com>
2025-03-03 01:51:35 +0000

    versal: add size-cells prop to gic fdt node
    size-cells property is not inherited from ancestors
    in the device tree, so must be explicitly defined in
    the GIC node.
    
    Devices that specify the GIC as the interrupt parent
    format the size field in their reg property according
    to the value of the GIC size-cells property.
    
    Without the size-cells property in the GIC node, the
    reg property of the pcie controller and other devices
    using the GIC as the interrupt parent may be parsed
    incorrectly, resulting in mmio regions being mapped
    incorrectly.
    
    Signed-off-by: Nelson Ho <Nelson.Ho@windriver.com>

--- hw/arm/xlnx-versal-virt.c
+++ hw/arm/xlnx-versal-virt.c
@@ -165,6 +165,7 @@ static void fdt_add_gic_nodes(VersalVirt *s)
                                  2, MM_GIC_APU_REDIST_0_SIZE);
     qemu_fdt_setprop_cell(s->fdt, nodename, "#interrupt-cells", 3);
     qemu_fdt_setprop_cell(s->fdt, nodename, "#address-cells", 2);
+    qemu_fdt_setprop_cell(s->fdt, nodename, "#size-cells", 2);
     qemu_fdt_setprop_string(s->fdt, nodename, "compatible", "arm,gic-v3");
 
     fdt_add_gic_its_node(s, nodename);
